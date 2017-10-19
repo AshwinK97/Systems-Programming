7ac07e97f4bc8441215e215a4f45a030884016f6
AshwinK97 <ashwin.kamalakannan@uoit.net>
2017-10-19 14:19:08 -0400

    finished most of lab2, added files for lab 3

--- Lab2/task2.c
+++ Lab2/task2.c
@@ -2,15 +2,30 @@
 #include <stdio.h>
 #include <string.h>
 
-// find and replace a character within a string
-char* find_replace(char* word, char* find, char* replace){
-	char *current_pos = strstr(word, find); // get pointer to first occurance of find
-	// loop as long as current positon is valid
-	while (current_pos) {
-		*current_pos = replace; // swap occurence of find with replace
-		current_pos = strstr(current_pos,find); // get next occurence of find
-	}
-	return word;
+// find all occurences of str, in stc and replace with rep
+void find_replace(char *text, char *find, char *replace)
+{
+	char *p = strstr(text, find); // get pointer to find in text
+    do {   
+        if(p) { // if p doesn't point to null
+            char buf[1500]; // temporary string
+            memset(buf,'\0',strlen(buf)); // add null terminator to end
+
+            // if the find ///////////////////////// CHANGE THIS
+            if(text == p) {
+                strcpy(buf,replace); // copy replace into buffer
+                strcat(buf,p+strlen(find)); // concat p to buffer
+            } else {
+                strncpy(buf,text,strlen(text) - strlen(p));
+                strcat(buf,replace);
+                strcat(buf,p+strlen(find));
+            }
+
+            memset(text,'\0',strlen(text));
+            strcpy(text,buf);
+        }   
+
+    } while(p && (p = strstr(text, find)));
 }
 
 int main(int argc, char *argv[]) {
@@ -29,20 +44,47 @@ int main(int argc, char *argv[]) {
 		exit(0);
 	}
 
-	// single array to hold entire paragraph
-	char text[1000];
+	char text[1500]; // single array to hold entire paragraph
+	int cc = 0; // tracks how many words are read
+	int swaps = 9; // how many words to swap
 
-	// track how many charcters are read
-	int cc = 0;
+	// array of words to find
+	char find[9][50] = {
+		"Inferno",
+		"In dark woods",
+		"those woods",
+		"to enter",
+		"crest",
+		"Below a hill",
+		"shoulders",
+		"planet",
+		"dante",
+	};
+
+	// array of words to replace
+	char replace[9][50] = {
+		"Paradisio",
+		"using Windows 8.1",
+		"Windows 8.1",
+		"to use 8.1",
+		"screen",
+		"Before a monitor",
+		"GUI",
+		"UNIX",
+		"Shakespeare",
+	};
 	
 	// read char from file until EOF
 	while ((text[cc] = getc(f)) != EOF)
 		cc++; // increment counter
-	
-	// use find and replace to replace all 'a' with 'b'
-	strcpy(text, find_replace(text, "The", "AAA"));
 
-	int i;
+	int i; // sentinal for loop
+	for (i = 0; i < swaps; i++) {
+		// use find and replace
+		find_replace(text, find[i], replace[i]);
+	}
+
+	// write new characters to file
 	for (i=0; i<cc; i++)
 		fputc(text[i], f2);
 	
