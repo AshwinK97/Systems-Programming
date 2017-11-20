9e755e20e2daca3760a6a7362ad5bed092d09f17
Ashwin <ashwin.kamalakannan@uoit.net>
2017-11-19 19:40:56 -0500

    finished assignment 2 part 1

--- Assignment2/duplicate.c
+++ Assignment2/duplicate.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <errno.h>
+
+// display message while duplicating
+void display_message(int s) {
+     printf("duplicate: still duplicating...\n" ); // display message
+     alarm(1); // set for 1 second
+     signal(SIGALRM, display_message); // setup another alarm
+}
 
 int main (int argc, char *argv[]) {
 	// check if arguments are correct
@@ -16,9 +25,9 @@ int main (int argc, char *argv[]) {
 	int src_fd = open(argv[1], O_RDONLY);
 	
 	// check if source file can be read
-	if (src_fd == -1) {
-		printf("duplicate: cannot find source file '%s'\n", argv[1]);
-		exit(src_fd);
+	if (src_fd < 0) {
+		printf("duplicate: '%s': %s\n", argv[1], strerror(errno)); // use errno to print error
+		exit(-1);
 	}
 	
 	// open or create new destination file for writing
@@ -26,14 +35,26 @@ int main (int argc, char *argv[]) {
 	
 	// check if destination file can be wrote
 	if (des_fd < 0) {
-		printf("duplicate: cannot write to destination file '%s'\n", argv[2]);
-		exit(des_fd);
+		printf("duplicate: '%s': %s\n", argv[2], strerror(errno)); // use errno to print error
+		exit(-1);
+	}
+	
+	char c; // character buffer for reading
+	int bytes = 0; // store the number of bytes read
+	
+	signal(SIGALRM, display_message); // set up alarm
+    alarm(1); // only display message after 1 second
+
+	// attempt to read 1 byte from source, loop as long as byte is read
+	while (read(src_fd, &c, 1) == 1) {
+		// attempt to write 1 byte to destination, if cannot write then disk is full
+		if (write(des_fd, &c, 1) != 1) {
+			printf("duplicate: %s\n", strerror(errno)); // use errno to print error
+			exit(-1);
+		}
+		bytes++; // increment the number of bytes read
 	}
 
-	printf("source fd: %d, destination fd: %d\n", src_fd, des_fd);
-	
-	int BUFF;
-	
-	
-	//File* dest_f = fdopen(src_fd, "w");
+	// output number of bytes copied from source to destination
+	printf("duplicate: copied %d bytes from '%s' to '%s'\n", bytes, argv[1], argv[2]);
 }
